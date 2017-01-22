e71f1fd9c6bdc26529b7d0ad83d904b799b8076a
Alan Gardner <urfolomeus@gmail.com>
2017-01-22 18:50:30 +0000

    Refactoring the JavaScript files to prepare for Redux

---
+++ apps/ryoc_web/web/static/js/app/containers/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './App.css'
+import cthulhu from '../../../assets/images/Cthulhu_blood.png'
+
+const App =
+  <div>
+    <h1>Hello, world!</h1>
+    <img src={cthulhu} />
+  </div>
+
+export default App
